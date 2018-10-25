a7a3baaa17fbe593307c8afa2970e3e741b93d55
Daniel Johnson <daniel_johnson@selinc.com>
2018-10-25 13:04:47 -0700

    Changes lock from slock to cinnamon-screensaver

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "/usr/bin/termite", NULL };
-static const char *slockcmd[]  = { "slock", NULL };
+static const char *slockcmd[]  = { "cinnamon-screensaver-command -l", NULL };
 static const char *upsndcmd[]  = { "ponymix", "increase", "5"};
 static const char *dwnsndcmd[]  = { "ponymix", "decrease", "5"};
 
